3a21acde01aa47f5140df6e9e3c6709eb891ecdb
Alex Lee <07alexlee@gmail.com>
2023-10-25 20:31:11 -0700

    Added to Commit class

--- Commit.java
+++ Commit.java
@@ -1,7 +1,33 @@
 public class Commit {
-    private String treeSha1;
+    private String treeSHA;
+    private String author;
+    private String date;
+    private String summary;
 
     public Commit (String treeSha1){
-        this.treeSha1 = treeSha1;
+        this.treeSHA = treeSHA;
+        this.author = author;
+        this.date = ;
+        this.summary = summary;
     }
+
+    public String getTreeSHA(){
+        return treeSHA;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public String createTree(){
+        Tree tree = new Tree();
+        return tree.generateSHA();
+    }
+
+    public String generateSHA(){
+        String file = summary + "\n" + date + "\n" + author + "\n" + treeSHA + "\n";
+        
+    }
+
+
 }
