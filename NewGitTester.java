f25c6f9a2d44fa88619437e7eb00fecac2ac1d2d
Alex Lee <07alexlee@gmail.com>
2023-10-30 23:36:14 -0700

    Edited tester bugs

--- NewGitTester.java
+++ NewGitTester.java
@@ -11,20 +11,12 @@ import org.junit.jupiter.api.DisplayName;
 public class NewGitTester {
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
-        /*
-         * Utils.writeStringToFile("junit_example_file_data.txt", "test file contents");
-         * Utils.deleteFile("index");
-         * Utils.deleteDirectory("objects");
-         */
+
     }
 
     @AfterAll
     static void tearDownAfterClass() throws Exception {
-        /*
-         * Utils.deleteFile("junit_example_file_data.txt");
-         * Utils.deleteFile("index");
-         * Utils.deleteDirectory("objects");
-         */
+
     }
 
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
@@ -43,7 +35,7 @@ public class NewGitTester {
         assert Files.exists(Paths.get("Git"));
         assert Files.exists(Paths.get("objects"));
 
-        System.out.println("init  success");
+        System.out.println("init success");
     }
 
     @Test
